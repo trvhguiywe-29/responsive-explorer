c2a1209140c57c88a5b35f46ebc85a91f9889046
Rholan <rturn5446@westada.org>
2025-04-25 15:26:12 +0000

    Added a button to the blog page

--- javascript/button.js
+++ javascript/button.js
@@ -1,3 +1,9 @@
 $(".showmail").click(function () {
     $(this).text("no you don't!!!")
 })
+
+$("#newblog").click(function () {
+    if ($(".aaaa").length == 0) {
+        $(".newblogs").append("<p class='aaaa'>asdfghjkl</p>")
+    }
+})
