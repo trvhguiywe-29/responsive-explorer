208ce34491be6eb989ae88e28f5b55de9997f556
Rholan <rturn5446@westada.org>
2025-04-29 14:44:32 +0000

    Made blog post form work

--- javascript/button.js
+++ javascript/button.js
@@ -5,9 +5,26 @@ $(".showmail").click(function () {
 $("#newblog").click(function () {
     if ($(".aaaa").length == 0) {
         $(".newblogs").append("<div class='aaaa'></div>")
-        $(".aaaa").append("Title: <br> <input></input> <br>")
-        $(".aaaa").append("Image Link: <br> <input></input> <br>")
-        $(".aaaa").append("Content: <br> <input></input> <br>")
-        $(".aaaa").append("<br> <button>Submit Content</button>")
+
+        $(".aaaa").append("Title: <br> <input id='title'></input> <br>")
+        $(".aaaa").append("Image Link: <br> <input id='imagelink'></input> <br>")
+        $(".aaaa").append("Content: <br> <input id='content'></input> <br>")
+        $(".aaaa").append("<br> <button id='submit'>Preview Blog Post</button><br>")
+
+        $(this).remove()
+        
     }
+    // $("#submit").text("aaaa")
+})
+
+$(document).on('click', '#submit', function () {
+    // $(this).remove()
+    console.log('this is working')
+
+    if ($("#createdblog").length == 0) {
+        $('.aaaa').append("<br> <div class='blogpost'> <div class='jumbotron' id='createdblog'</div> </div>");
+        $("#createdblog").append("<h2 class='roboto'>" + $("#title").val() + "</h2>")
+        $("#createdblog").append("<div class='row'> <img src='" + $("#imagelink").val() + "' class='col-sm-4' /> <p class='col-sm-8'>" + $("#content").val() + "</p> </div>")
+    }
+    
 })
