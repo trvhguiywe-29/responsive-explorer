79c923f8e220bb13d7df5be5fe8fb24fec815cf2
Rholan <rturn5446@westada.org>
2025-04-29 14:04:04 +0000

    Added text inputs to the create blog post button

--- javascript/button.js
+++ javascript/button.js
@@ -4,6 +4,10 @@ $(".showmail").click(function () {
 
 $("#newblog").click(function () {
     if ($(".aaaa").length == 0) {
-        $(".newblogs").append("<p class='aaaa'>asdfghjkl</p>")
+        $(".newblogs").append("<div class='aaaa'></div>")
+        $(".aaaa").append("Title: <br> <input></input> <br>")
+        $(".aaaa").append("Image Link: <br> <input></input> <br>")
+        $(".aaaa").append("Content: <br> <input></input> <br>")
+        $(".aaaa").append("<br> <button>Submit Content</button>")
     }
 })
